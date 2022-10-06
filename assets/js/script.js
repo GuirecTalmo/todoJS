ec8e7a918e156eaa25fc9d6acc4452729c3e7a47
Guirec Talmo <guirec.talmo@gmail.com>
2022-10-06 17:00:15 +0200

    Rework CSS : CSS from scratch has been removed. Tailwind CSS add to the project

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,7 +38,7 @@ function renderTodos(todos) {
 
     const li = document.createElement("li");
 
-    li.setAttribute("class", "item");
+    li.setAttribute("class", "item flex items-center");
 
     li.setAttribute("data-key", item.id);
 
@@ -48,7 +48,12 @@ function renderTodos(todos) {
     li.innerHTML = `
       <input type="checkbox" class="checkbox" ${checked}>
       ${item.name}
-      <button class="delete-button">X</button>
+      <button
+              type="button"
+              class="delete-button text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-purple-800 font-medium rounded-full text-sm px-4 py-2.5 text-center ml-auto"
+            >
+              X
+            </button>
     `;
 
     todoItemsList.append(li);
---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  theme: {
+    screens: {
+      mobile: "640px",
+      tablet: "960px",
+      desktop: "1280px",
+    },
+    container: {
+      padding: "2rem",
+      screens: {
+        mobile: "600px",
+        tablet: "900px",
+        desktop: "1200px",
+      },
+    },
+  },
+};
